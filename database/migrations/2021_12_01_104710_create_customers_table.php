efa88f60a39d9fa7bac8a3fa9da88c7cdf5b6ab7
Prabin Kharel <prabinkharel@192-168-1-105.tpgi.com.au>
2021-12-03 12:39:36 +1100

    First Commit

---
+++ database/migrations/2021_12_01_104710_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->integer('postal_code');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
